85ae9a0c103d374217fb92ce7db44217529abca6
Stephan Finkensieper <stephan.finkensieper@dwd.de>
2020-09-16 14:02:42 +0200

    Fix issue creation

--- tools/create-github-issue.sh
+++ tools/create-github-issue.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# Create a pytroll/pygac-fdr issue with the given title and body. This has been moved to a
+# separate script because commands containing colons invalidate travis.yaml .
 set -e
 if [ "$#" -ne 2 ]; then
     echo "Usage: create-github-issue <title> <body>"
@@ -7,4 +8,4 @@ if [ "$#" -ne 2 ]; then
 fi
 title=$1
 body=$2
-curl -u $GITHUB_TOKEN:x-oauth-basic -i -H "Content-Type: application/json" -X POST --data '{"title":"'$title'", "body":"'$body'"}' https://api.github.com/repos/pytroll/pygac-fdr/issues
+curl -u $GITHUB_TOKEN:x-oauth-basic -i -H "Content-Type: application/json" -X POST --data "{\"title\":\"${title}\", \"body\":\"${body}\"}" https://api.github.com/repos/pytroll/pygac-fdr/issues
