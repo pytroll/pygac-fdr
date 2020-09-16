a68f772de65722fc6e1551e8988a4f9ef91b226a
Stephan Finkensieper <stephan.finkensieper@dwd.de>
2020-09-16 14:31:41 +0200

    Fix issue creation

--- tools/create-github-issue.sh
+++ tools/create-github-issue.sh
@@ -8,4 +8,4 @@ if [ "$#" -ne 2 ]; then
 fi
 title="$1"
 body="$2"
-curl -u $GITHUB_TOKEN:x-oauth-basic -i -H "Content-Type: application/json" -X POST --data "{\"title\":\"$title\", \"body\":\"$body\"}" https://api.github.com/repos/pytroll/pygac-fdr/issues/
+curl -u $GITHUB_TOKEN:x-oauth-basic -i -H "Content-Type: application/json" -X POST --data "{\"title\":\"$title\", \"body\":\"$body\"}" https://api.github.com/repos/pytroll/pygac-fdr/issues
