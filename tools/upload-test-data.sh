9bd5356308b4d73513876fb6ff386a166e02a360
Stephan Finkensieper <stephan.finkensieper@dwd.de>
2020-09-21 16:11:02 +0200

    Add script to upload test data

---
+++ tools/upload-test-data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Upload test data to DWD server.
+
+set -e
+
+upload='/cmsaf/nfshome/routcm/common/bin/put_public_cmsaf'
+
+# Delete existing data
+#$upload -D perm/pygac-fdr/test_data/
+#sleep 30
+
+# Upload new data
+$upload -p perm/pygac-fdr/test_data/input/corrupt input/corrupt/NSS*
+sleep 30
+$upload -p perm/pygac-fdr/test_data/input/normal input/normal/*.gz
+sleep 30
+$upload -p perm/pygac-fdr/test_data/tle tle/*.txt
+sleep 10
+$upload -p perm/pygac-fdr/test_data/output_ref/normal output_ref/normal/*.nc
+sleep 30
+$upload -p perm/pygac-fdr/test_data/output_ref/corrupt output_ref/corrupt/*.nc
