5745e00f396960bef046d0d279ac3623ec705a97
Stephan Finkensieper <stephan.finkensieper@dwd.de>
2020-09-21 17:26:45 +0200

    Fix uploader

--- tools/upload-test-data.sh
+++ tools/upload-test-data.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 # Upload test data to DWD server.
 
-set -e
-
 upload='/cmsaf/nfshome/routcm/common/bin/put_public_cmsaf'
 
 # Delete existing data
-#$upload -D perm/pygac-fdr/test_data/
-#sleep 30
+$upload -D perm/pygac-fdr/test_data/
+sleep 30
 
 # Upload new data
+set -e
 $upload -p perm/pygac-fdr/test_data/input/corrupt input/corrupt/NSS*
 sleep 30
 $upload -p perm/pygac-fdr/test_data/input/normal input/normal/*.gz
